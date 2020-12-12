4d606eb12b90292237247674cbefd2e197f6ea99
Joeri Minnekeer <joeri@MacBook-Pro-van-Joeri.local>
2020-12-12 12:08:43 +0100

    latest version

--- pages/index.vue
+++ pages/index.vue
@@ -3,46 +3,41 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 export default {
-
-  head (){
+  head() {
     return {
       title: this.page.meta_title,
       meta: [
         {
-          hid: 'description',
-          name: 'description',
-          content: this.page.meta_description
+          hid: "description",
+          name: "description",
+          content: this.page.meta_description,
         },
         {
-          property: 'og:title',
-          content: this.page.og_title
+          property: "og:title",
+          content: this.page.og_title,
         },
         {
-          property: 'og:description',
-          content: this.page.og_description
+          property: "og:description",
+          content: this.page.og_description,
         },
         {
-          property: 'og:image',
-          content: this.page.og_image.url
+          property: "og:image",
+          content: this.page.og_image.url,
         },
-      ]
-    }
+      ],
+    };
   },
   data() {
     return {
-      page:[]
-    }
+      page: [],
+    };
   },
   async fetch() {
     const homeData = await this.$prismic.api.getByUID("pagina", "home");
     this.page = homeData.data;
-  }
+  },
 };
-=======
-export default {};
->>>>>>> 1995f96f097dff28b55e3a580d729ff5c5e3470f
 </script>
 
 <style scoped>
